8b7062b0642a7864fbd752b835f2eda0aefa737b
Latestinssan <161745695+Preet3627@users.noreply.github.com>
2026-03-26 20:14:26 +0530

    Add closeAuthWindow helper to preload auth API

--- comet-browser/preload.js
+++ comet-browser/preload.js
@@ -134,6 +134,7 @@ contextBridge.exposeInMainWorld('electronAPI', {
 
   // Auth
   openAuthWindow: (url) => ipcRenderer.send('open-auth-window', url),
+  closeAuthWindow: () => ipcRenderer.send('close-auth-window'),
   onAuthCallback: (callback) => {
     const subscription = (event, url) => callback(event, url);
     ipcRenderer.on('auth-callback', subscription);
